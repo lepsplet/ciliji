b0d22890a50ed1649bef450abbad2421635f65dd
Tonček Balonček <toni.cesarek@gmail.com>
2014-04-23 08:09:26 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CiljiApp::Application.config.secret_key_base = 'd5a322a44c2586573db4ded903b29b7c0d9f7a89761af80b01b35469ad14b29812a4c869ac5af663799c4d9f4598190ff326099153029005111a4dfb4e03edc5'
